5d4cc2e680f3a7247e45bed6c1d4ab77ae3e52ba
mangming2 <59263564+mangming2@users.noreply.github.com>
2022-09-02 21:00:22 +0900

    Test: login by metamask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <>크립토 타투 메인페이지입니다</>;
+  const [account, setAccount] = useState<string>("");
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+      } else {
+        alert("install metamask");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAccount();
+  }, [account]);
+
+  return (
+    <>
+      <div>크립토 타투 메인페이지입니다{account}</div>
+    </>
+  );
 };
 
 export default Home;
