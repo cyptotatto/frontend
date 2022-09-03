558c403bd03b72e79a203ab011040ea09e2ba8b7
INBUM02 <kib0202@naver.com>
2022-09-03 11:19:07 +0900

    FEAT: minting 페이지에 사진업로드 및 미리보기 구현

--- pages/minting.tsx
+++ pages/minting.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 
-function minting() {
-  return <div>minting</div>;
+function Minting() {
+  const Ref = useRef<any>();
+  const [img, setImage] = useState<any>();
+  const [imgSrc, setImgSrc] = useState<any>("");
+
+  const clickInput = () => {
+    Ref.current.click();
+  };
+
+  const imgUpload = (e: any) => {
+    setImage(e.target.files[0]);
+  };
+
+  const onSubmit = () => {};
+
+  const encodeFileToBase64 = (fileBlob: any) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve: any) => {
+      reader.onload = async () => {
+        console.log(reader.result);
+        setImgSrc(reader.result);
+        resolve();
+      };
+    });
+  };
+
+  useEffect(() => {
+    img && encodeFileToBase64(img);
+  }, [img]);
+
+  return (
+    <>
+      <button onClick={clickInput}>사진업로드</button>
+      <input
+        ref={Ref}
+        accept="image/"
+        type="file"
+        onChange={imgUpload}
+        style={{ display: "none" }}
+      ></input>
+      <button onClick={onSubmit}>보내기</button>
+      {imgSrc ? (
+        <Image alt="사진" src={imgSrc} width="100" height="100" />
+      ) : (
+        <div>미리보기</div>
+      )}
+    </>
+  );
 }
 
-export default minting;
+export default Minting;
