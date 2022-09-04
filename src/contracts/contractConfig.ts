367ecdaad6c3b26d4f2665b546e9e60caebf80f3
INBUM02 <kib0202@naver.com>
2022-09-04 21:35:02 +0900

    FEAT: contract config 설정

---
+++ src/contracts/contractConfig.ts
@@ -0,0 +1,10 @@
+import { AbiItem } from "web3-utils";
+import Web3 from "web3";
+
+const contractABI: AbiItem[] = [];
+
+const contractAddress = "주소자리";
+
+export const web3 = new Web3(window.ethereum);
+
+export const contract = new web3.eth.Contract(contractABI, contractAddress);
