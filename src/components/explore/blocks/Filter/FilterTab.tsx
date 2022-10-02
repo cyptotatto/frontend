486f68c9709523589f22bbfd9aaa239365db2ee7
Jisu00 <jisudev00@gmail.com>
2022-10-02 18:59:56 +0900

    FEAT: Filter 메뉴 toggle 기능 추가

---
+++ src/components/explore/blocks/Filter/FilterTab.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function FilterTab() {
+  return <div></div>;
+}
+
+export default FilterTab;
--- src/components/explore/blocks/NavBar.tsx
+++ src/components/explore/blocks/NavBar.tsx
@@ -1,73 +1,22 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 
-import TabMenu from "../atoms/NavBar/TabMenu";
-import FilterIcon from "../../../../public/assets/filter.svg";
-
-type UnderLinePropsType = {
-  selectedMenu: string;
-};
+import FilterMenuBox from "./Filter/FilterMenuBox";
+import SelectMenuBox from "./Select/SelectMenuBox";
 
 function NavBar() {
-  const [selectedMenu, setSelectedMenu] = useState("도안");
-  const [isFilterOpen, setIsFilterOpen] = useState(false);
-
-  const onToggleFilter = () => setIsFilterOpen((prev) => !prev);
-  const onChangeMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setSelectedMenu(e.currentTarget.innerText);
-  };
-
   return (
-    <StyledNav>
-      <StyledFilterIcon
-        onClickHandler={onToggleFilter}
-        style={{ verticalAlign: "middle" }}
-      />
-      <SelectMenuBox>
-        <TabMenu
-          isActive={selectedMenu === "도안"}
-          onClickHandler={onChangeMenu}
-        >
-          도안
-        </TabMenu>
-        <TabMenu
-          isActive={selectedMenu === "아티스트"}
-          onClickHandler={onChangeMenu}
-        >
-          아티스트
-        </TabMenu>
-        <UnderLine selectedMenu={selectedMenu} />
-      </SelectMenuBox>
-    </StyledNav>
+    <Nav>
+      <FilterMenuBox />
+      <SelectMenuBox />
+    </Nav>
   );
 }
 
 export default NavBar;
 
-const StyledNav = styled.nav`
+const Nav = styled.nav`
   display: flex;
   align-items: center;
   border-bottom: 1px solid #7a7a7a;
-`;
-
-const StyledFilterIcon = styled(FilterIcon)`
-  margin: 12px 16px;
-  cursor: pointer;
-`;
-
-const SelectMenuBox = styled.div`
-  position: relative;
-`;
-
-const UnderLine = styled.div<UnderLinePropsType>`
-  position: absolute;
-  left: ${(props) => {
-    if (props.selectedMenu === "도안") return "0px";
-    else return "80px";
-  }};
-  transition: 0.1s ease-out;
-  bottom: -2px;
-  height: 3px;
-  width: 80px;
-  background: linear-gradient(90deg, #d1374e 18.55%, #ac2aa1 96.67%);
 `;
