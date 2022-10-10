dbbaed151cb3e4f1a142643b9ca8b9a67b884b30
Jisu00 <jisudev00@gmail.com>
2022-10-10 18:19:46 +0900

    DESIGN: Figure Box 컴포넌트 추가

---
+++ src/components/artist/atoms/Figure.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+import { makeFormattedNumber } from "../../../utils/transform";
+import EthereumIcon from "../../../../public/assets/ethereum.svg";
+
+interface IProps {
+  title: string;
+  num: number | string;
+  isEthereum: boolean;
+}
+
+function Figure({ title, num, isEthereum }: IProps) {
+  return (
+    <Box>
+      <span className="title">{title}</span>
+      <div className="numBox">
+        {isEthereum && <EthereumIcon />}
+        <span>{typeof num === "number" ? makeFormattedNumber(num) : num}</span>
+      </div>
+    </Box>
+  );
+}
+
+export default Figure;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 32px;
+
+  .title {
+    color: #7a7a7a;
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  .numBox {
+    font-size: 24px;
+    font-weight: 700;
+  }
+`;
