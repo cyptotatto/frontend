bdc4ba3cae2b37ccc2278387d979f4ed2345ec92
mangming2 <leejiho35@gmail.com>
2022-11-22 01:03:29 +0900

    Update NavBar.tsx

--- src/components/mypage/blocks/NavBar.tsx
+++ src/components/mypage/blocks/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import CustomNavBar from "../../common/CustomNavBar";
@@ -7,10 +7,15 @@ import SelectedMenuBox from "../../common/CustomNavBar/blocks/SelectMenuBox";
 const myPageMenu = ["소유 NFT", "만든 NFT", "판매한 NFT", "좋아요한 NFT"];
 
 function NavBar() {
+  const [selectedMenuIdx, setSelectedMenuIdx] = useState(0);
   return (
     <CustomNavBar>
       <Layout>
-        <SelectedMenuBox menu={myPageMenu} />
+        <SelectedMenuBox
+          menu={myPageMenu}
+          selectedMenuIdx={selectedMenuIdx}
+          setSelectedMenuIdx={setSelectedMenuIdx}
+        />
       </Layout>
     </CustomNavBar>
   );
