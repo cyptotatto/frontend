1432591f39803af198c878ba27823f391aa13740
mangming2 <leejiho35@gmail.com>
2023-03-08 10:07:23 +0900

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "http://ec2-43-200-255-169.ap-northeast-2.compute.amazonaws.com:4000",
-  timeout: 3000,
+  baseURL: "http://ec2-43-200-255-169.ap-northeast-2.compute.amazonaws.com:80",
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
